716e4e4909017fe2b800edacfa6dd0546b11d0fa
Andrew Kairys <andrewkairys@gmail.com>
2018-03-07 16:52:08 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,16 @@
-def my_each(collection)
-  while collection.count > 0
-    {|i| puts i}
+# def my_each(collection)
+#   while collection.count > 0
+#     {|i| puts i}
+#   end
+# end
+
+def my_each(array)
+  i = 0
+
+  while i < array.count
+    yield array[i]
+    i += 1
   end
+
+  array
 end
